47a3051ad3a870bbc6e993248f15605bf5359bda
MahmoudAshraf97 <hassouna97.ma@gmail.com>
2023-10-06 02:48:21 +0300

    enable fully offline processing by removing msdd config file download each time

--- helpers.py
+++ helpers.py
@@ -43,14 +43,16 @@ wav2vec2_langs = [
 
 
 def create_config(output_dir):
-    DOMAIN_TYPE = "telephonic"  # Can be meeting or telephonic based on domain type of the audio file
+    DOMAIN_TYPE = "telephonic"  # Can be meeting, telephonic, or general based on domain type of the audio file
+    CONFIG_LOCAL_DIRECTORY = "nemo_msdd_configs"
     CONFIG_FILE_NAME = f"diar_infer_{DOMAIN_TYPE}.yaml"
-    CONFIG_URL = f"https://raw.githubusercontent.com/NVIDIA/NeMo/main/examples/speaker_tasks/diarization/conf/inference/{CONFIG_FILE_NAME}"
-    MODEL_CONFIG = os.path.join(output_dir, CONFIG_FILE_NAME)
-    if not os.path.exists(MODEL_CONFIG):
-        MODEL_CONFIG = wget.download(CONFIG_URL, output_dir)
+    MODEL_CONFIG_PATH = os.path.join(CONFIG_LOCAL_DIRECTORY, CONFIG_FILE_NAME)
+    if not os.path.exists(MODEL_CONFIG_PATH):
+        os.makedirs(CONFIG_LOCAL_DIRECTORY, exist_ok=True)
+        CONFIG_URL = f"https://raw.githubusercontent.com/NVIDIA/NeMo/main/examples/speaker_tasks/diarization/conf/inference/{CONFIG_FILE_NAME}"
+        MODEL_CONFIG_PATH = wget.download(CONFIG_URL, MODEL_CONFIG_PATH)
 
-    config = OmegaConf.load(MODEL_CONFIG)
+    config = OmegaConf.load(MODEL_CONFIG_PATH)
 
     data_dir = os.path.join(output_dir, "data")
     os.makedirs(data_dir, exist_ok=True)
