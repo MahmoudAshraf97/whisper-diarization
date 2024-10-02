360bd10d447fe6452c8af8e364255b5975cdb3b3
MahmoudAshraf97 <hassouna97.ma@gmail.com>
2024-10-02 13:27:50 +0300

    remove `whisperx`

--- transcription_helpers.py
+++ transcription_helpers.py
@@ -43,28 +43,3 @@ def transcribe(
     del whisper_model
     torch.cuda.empty_cache()
     return whisper_results, info.language
-
-
-def transcribe_batched(
-    audio_file: str,
-    language: str,
-    batch_size: int,
-    model_name: str,
-    compute_dtype: str,
-    suppress_numerals: bool,
-    device: str,
-):
-    import whisperx
-
-    # Faster Whisper batched
-    whisper_model = whisperx.load_model(
-        model_name,
-        device,
-        compute_type=compute_dtype,
-        asr_options={"suppress_numerals": suppress_numerals},
-    )
-    audio = whisperx.load_audio(audio_file)
-    result = whisper_model.transcribe(audio, language=language, batch_size=batch_size)
-    del whisper_model
-    torch.cuda.empty_cache()
-    return result["segments"], result["language"], audio
